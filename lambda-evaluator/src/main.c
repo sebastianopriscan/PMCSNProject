f9dc2ca0876c3f3fee622217a9904bb7bef6c750
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2023-11-09 19:22:52 +0100

    Tried changing the way wait times are calculated due to discrepancies with theoretical results. Proper data structure should be added

--- lambda-evaluator/src/main.c
+++ lambda-evaluator/src/main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <simulation/simulation.h>
 #include <rngs.h>
 #include <rvgs.h>
+
+double arrival[40960] = {0.0} ; 
+
+
+char *rides[12] = {"Alice In Wonderland",
+                   "Casey Junior's Circus Tale",
+                   "Dumbo",
+                   "Small World",
+                   "King Arthur",
+                   "Mad tea party",
+                   "Matterhorn",
+                   "Mister toad",
+                   "Peter Pan",
+                   "Pinocchio",
+                   "Snow White",
+                   "Storybook" } ;
+
+double expected_waits[12] = {34.0,
+                            15.0,
+                            28.0,
+                            19.0,
+                            18.0,
+                            17.0,
+                            52.0,
+                            20.0,
+                            39.0,
+                            17.0,
+                            22.0,
+                            16.0} ;
+
+double mus[12] = {4.0,
+                  4.0,
+                  2.0,
+                  14.0,
+                  3.0,
+                  2.0,
+                  4.0,
+                  2.0,
+                  3.0,
+                  3.0,
+                  2.0,
+                  10.0} ;
+
+int ridesNum[12] = {16,
+                    20,
+                    16,
+                    16,
+                    68,
+                    18,
+                    120,
+                    32,
+                    18,
+                    24,
+                    16,
+                    14} ;
 
 struct simulation_state {
   double lmba ;
@@ -13,6 +69,8 @@ struct simulation_state {
   unsigned int n_of_Servers ;
   unsigned char *servers_status ;
 
+  unsigned int total_Clients_arrived ;
+
   unsigned int total_clients_normal;
   unsigned int total_clients_vip;
   double total_service_time_normal;
@@ -20,8 +78,6 @@ struct simulation_state {
   double total_delay_normal;
   double total_delay_vip;
 } ;
-
-void server_activate(struct simulation *sim, void *metadata) ;
 
 struct simulation_state *init_state(unsigned int servNum, double lmba, double mu, double sigma)
 {
@@ -49,6 +105,9 @@ struct simulation_state *init_state(unsigned int servNum, double lmba, double mu
   val->total_delay_vip = 0.0;
   val->total_service_time_normal = 0.0;
   val->total_service_time_vip = 0.0;
+
+  val->total_Clients_arrived = 0 ;
+
   for (int i = 0; i < servNum; i++)
   {
     val->servers_status[i] = 0;
@@ -66,13 +125,14 @@ void destroy_state(struct simulation_state *state)
 void server_activate(struct simulation *sim, void *metadata)
 {
   int i = (int) metadata;
-  SelectStream(i +1) ;
+  SelectStream(i + 1) ;
   struct simulation_state *state = (struct simulation_state*) sim->state;
 
   double service_time = 0.0;
   
   if (state ->vip_Clients >0 || state->n_Clients > 0)
-   service_time = Normal(state->mu, state->sigma) ;
+    service_time = Exponential(state->mu) ;
+  //  service_time = Normal(state->mu, state->sigma) ;
 
   if(state->vip_Clients > 0) {
     state->vip_Clients -= 1;
@@ -108,13 +168,14 @@ void arrivalPayload(struct simulation *sim, void *metadata) {
     double p = Uniform(0.0, 1.0);
     if (p > 0.5) {
       state->vip_Clients += 1;
-      state->total_delay_vip -= sim->clock ;
-      state->total_delay_vip += sim->simEnd;
+      // state->total_delay_vip -= sim->clock ;
+      // state->total_delay_vip += sim->simEnd;
     }else {
       state->n_Clients += 1 ;
-      state->total_delay_normal -= sim->clock ;
-      state->total_delay_normal += sim->simEnd;
+      // state->total_delay_normal -= sim->clock ;
+      // state->total_delay_normal += sim->simEnd;
     }
+    state->total_Clients_arrived += 1;
 
     for(int i = 0 ; i < state->n_of_Servers ; i++)
     {
@@ -140,7 +201,7 @@ void next_arrival(struct simulation *sim, void *metadata) {
     enqueue_event(sim->queues->queue, event1) ;
 }
 
-struct simulation_state *run_single_simulation(double lambda, double mu, int server_num) {
+struct simulation *run_single_simulation(double lambda, double mu, int server_num) {
     struct simulation_state* state = init_state(server_num, lambda, mu, 0.1 * mu);
     PlantSeeds(12345);
     simulation *sim = create_simulation(sizeof(struct simulation_state) + sizeof(char) * server_num, server_num + 1, 960, (char *) state);
@@ -148,31 +209,50 @@ struct simulation_state *run_single_simulation(double lambda, double mu, int ser
     enqueue_event(sim->queues->queue, event);
     run_simulation(sim);
     destroy_state(state);
-    state = (struct simulation_state*)(sim->state);
-    return state;
+    return sim ;
 }
 
 double run_lambda_evaluator(double expected_wait, double threshold, double mu, int server_num) {
   double lambda = 0.1;
   double checker = 0.0;
+  double result = 0.0;
   do {
-    lambda += 0.01;
-    struct simulation_state *state = run_single_simulation(lambda, mu, server_num);
-    double result = (state->total_delay_normal + state->total_delay_vip) / (state->total_clients_normal + state->total_clients_vip);
+    lambda += 0.005;
+    struct simulation *sim = run_single_simulation(lambda, mu, server_num);
+    struct simulation_state *state = (struct simulation_state*)(sim->state);
+    double total_arrival = 0.0;
+    for (int i = 0; i < state->total_Clients_arrived; i++)
+    {
+      total_arrival += arrival[i];
+    }
+    double total_delay = state->total_delay_normal + state->total_delay_vip - total_arrival;
+    result = total_delay / (state->total_clients_normal + state->total_clients_vip);
+    // if (result != 0.0)
+    //   printf("\tLambda: %.4f, Wait: %.6f; %d\n", lambda, result, result < expected_wait);
     checker = (result - expected_wait) > 0 ? result - expected_wait : expected_wait - result ;
-   } while (checker > threshold);
+    destroy_simulation(sim);
+   } while (checker > threshold && result < expected_wait);
    return lambda;
 }
 
 int main(void) {
-  double lambda = run_lambda_evaluator(30.0, 0.5, 4.0, 16);
-  printf("Lambda: %6.6f\n", lambda);
-  struct simulation_state *state = run_single_simulation(lambda, 4.0, 16);
-  printf("Average Queue Time (normal): %6.6f\n", state->total_delay_normal / state->total_clients_normal);
-  printf("Average Queue Time (vip): %6.6f\n", state->total_delay_vip / state->total_clients_vip);
-  printf("Average Queue Time: %6.6f\n", (state->total_delay_normal + state->total_delay_vip) / (state->total_clients_normal + state->total_clients_vip));
-  printf("Average Service Time (normal): %6.2f\n", state->total_service_time_normal / state->total_clients_normal);
-  printf("Average Service Time (vip): %6.2f\n", state->total_service_time_vip / state->total_clients_vip);
-  printf("Average Service Time: %6.2f\n", (state->total_service_time_normal + state->total_service_time_vip) / (state->total_clients_normal + state->total_clients_vip));
+
+  for(int i = 0; i < 12; i++)
+  {
+    double lambda = run_lambda_evaluator(expected_waits[i], 0.5, mus[i], 1);
+    fprintf(stderr, "%s : Lambda: %6.6f, Expected Time %6.6f\n", rides[i], lambda, expected_waits[i]);
+    printf("%.4f, %.4f\n", lambda, mus[i]);
+    struct simulation_state *state = run_single_simulation(lambda, mus[i], 1) ;
+    double total_arrival = 0.0;
+    for (int i = 0; i < state->total_Clients_arrived; i++)
+    {
+      total_arrival += arrival[i];
+    }
+    double total_delay = state->total_delay_normal + state->total_delay_vip - total_arrival;
+    double wait = total_delay / (state->total_clients_normal + state->total_clients_vip);
+    fprintf(stderr, "\tAverage Queue Time: %6.6f\n", wait);
+    fprintf(stderr, "\tNumber of clients: %d\n", state->total_clients_normal + state->total_clients_vip);
+    fprintf(stderr, "\tTotal Clients Arrived: %d\n", state->total_Clients_arrived);
+  }
   return 0;
 }
