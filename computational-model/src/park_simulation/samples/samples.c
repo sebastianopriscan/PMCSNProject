eb5b04d6b2dcc7a4a06f967f277758fa5b282d7b
Alessandro Lioi <lioialessandro2000@live.com>
2024-02-19 15:53:00 +0100

    Fix sampling statistics: now it's considering also the clients in the queues

--- computational-model/src/park_simulation/samples/samples.c
+++ computational-model/src/park_simulation/samples/samples.c
@@ -1,6 +1,8 @@
 #include "samples.h"
 #include <stdio.h>
 #include "../sim_state.h"
+#include "../behaviors/behaviors.h"
+#include "generic_queue.h"
 
 void collect_stats(struct simulation *sim, void *metadata) {
 
@@ -14,6 +16,18 @@ void collect_stats(struct simulation *sim, void *metadata) {
 
     for (int i = 0; i < sim_state->park->num_rides; i++) {
       struct ride_state ride = sim_state->rides[i];
+      struct generic_queue_node* node = ride.vip_queue->head;
+      while (node != NULL) {
+          struct client_event *client_ev = (struct client_event*) node->data;
+          sum_delay_vip += client_ev->arrival_time - sim->clock;
+          node = node->next;
+      }
+      node = ride.normal_queue->head;
+      while (node != NULL) {
+          struct client_event *client_ev = (struct client_event*) node->data;
+          sum_delay_normal += client_ev->arrival_time - sim->clock;
+          node = node->next;
+      }
       sum_delay_normal += ride.total_delay_normal;
       sum_delay_vip += ride.total_delay_vip;
       sum_normal += ride.total_arrived_normal;
